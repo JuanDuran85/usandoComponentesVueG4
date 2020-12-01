2156581f4a18b84fd455e991559b9f171fa94dc7
JuanDuran85 <duran85juan@gmail.com>
2020-12-01 15:16:23 -0300

    ultimos cambios

---
+++ src/components/Lista.vue
@@ -0,0 +1,42 @@
+<template>
+<section class="container">
+    <div v-for="(item, index) in dataUser" :key="index">
+      <div>
+      <img :src="item.src" :alt="item.nombre" class="img-fluid mt-4">
+      </div>
+      <div>
+          <div id="resultados" class="resultados row mx-auto text-center">
+            <p>{{item.nombre}}</p>
+          </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'Lista',
+  props: {
+    dataUser: {
+      type: Array,
+      required: true,
+      default(){
+        return [
+          {nombre: 'no tiene', src: 'no tiene'}
+        ]
+      }
+    }
+  },
+
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  img {
+    border-radius: 50%;
+    margin-left: 20px;
+    width: 200px;
+    
+  }
+</style>
---
+++ src/components/Main.vue
@@ -0,0 +1,41 @@
+<template>
+<main>
+    <header class="text-white">
+        <Lista :dataUser="personajes"/>
+    </header>
+  </main>
+</template>
+
+<script>
+import Lista from './Lista.vue';
+
+export default {
+  name: 'Main',
+  data() {
+      return {
+        personajes: [
+            {nombre: "Rick Sanchez", src:"https://rickandmortyapi.com/api/character/avatar/1.jpeg"},
+            {nombre: "Morty Smith", src:"https://rickandmortyapi.com/api/character/avatar/2.jpeg"},
+            {nombre: "Summer Smith", src:"https://rickandmortyapi.com/api/character/avatar/3.jpeg"},
+            {nombre: "Beth Smith", src:"https://rickandmortyapi.com/api/character/avatar/4.jpeg"},
+            {nombre: 	"Alien Googah", src:"https://rickandmortyapi.com/api/character/avatar/13.jpeg"},
+            {nombre: "Adjudicator Rick", src:"https://rickandmortyapi.com/api/character/avatar/8.jpeg"},
+        ]
+      }
+  },
+  components:{
+      Lista
+  }
+}
+</script>
+
+<style scoped>
+    header {
+        margin: 0;
+        padding: 0;
+        background-image: url('https://rickandmortypod.com/wp-content/uploads/2018/11/cropped-RM_page-header_background1-3.png');
+        background-repeat: no-repeat;
+        background-size: cover;
+        background-position: center top; 
+    }
+</style>
